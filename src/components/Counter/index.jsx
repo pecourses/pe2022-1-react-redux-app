9f5ba3cd668e25569855448f23047611e0dca6ec
Viktoriia <pecourses>
2023-01-22 20:35:45 +0200

    Refactor Counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
+
 import { connect } from 'react-redux';
 import { decrement, increment } from '../..';
 
 function Counter (props) {
   const { count, dispatch } = props;
 
+  const decrementCount = () => {
+    dispatch(decrement());
+  };
+
+  const incrementCount = () => {
+    dispatch(increment());
+  };
+
   return (
     <div>
       <div>{count}</div>
-      <button
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        -
-      </button>
-      <button
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        +
-      </button>
+      <button onClick={decrementCount}>-</button>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
