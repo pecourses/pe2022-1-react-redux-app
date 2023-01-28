70b6c58dc63c20c9898fb435d0e5842dd8d81dd1
Viktoriia <pecourses>
2023-01-28 16:04:47 +0200

    Fix Counter imports

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
-import { decrement, increment } from '../..';
+import { decrement, increment } from './../../store/slices/counterSlice';
 
 function Counter (props) {
   const { count, dispatch } = props;
